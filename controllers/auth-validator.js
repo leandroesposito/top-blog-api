9870570b12fe89aa8a732406019b9b1d0281252c
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-18 20:33:21 -0300

    Create auth validator

---
+++ controllers/auth-validator.js
@@ -0,0 +1,36 @@
+import passport from "passport";
+
+function authenticate(requireAuthor = true) {
+  return function (req, res, next) {
+    return passport.authenticate(
+      "jwt",
+      { session: false },
+      (error, user, info) => {
+        if (error) {
+          console.error("Auth error:", error);
+          next(error);
+        }
+
+        if (!user) {
+          return res.status(401).json({
+            errors: ["Not authorized, invalid token"],
+          });
+        }
+
+        if (requireAuthor && !user.isAuthor) {
+          return res.status(403).json({
+            errors: [
+              "Not authorized, you must be an author to access this resource",
+            ],
+          });
+        }
+
+        req.user = user;
+
+        next();
+      }
+    )(req, res, next);
+  };
+}
+
+export default authenticate;
