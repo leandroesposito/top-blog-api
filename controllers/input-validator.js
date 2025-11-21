3859cbf011dfb3e42a6d3a0089a91a3598b6ad85
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-21 18:32:48 -0300

    Send response when validations fail

--- controllers/input-validator.js
+++ controllers/input-validator.js
@@ -3,7 +3,7 @@ import { validationResult } from "express-validator";
 function checkValidations(req, res, next) {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    res.locals.errors = errors.array().map((e) => e.msg);
+    return res.status(409).json({ errors: errors.array().map((e) => e.msg) });
   }
   next();
 }
