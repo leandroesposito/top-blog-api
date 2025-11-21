e470daf317cd9f1b2481f20facf437009e383f51
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-11-21 18:41:05 -0300

    Create post DB model

---
+++ db/post.js
@@ -0,0 +1,76 @@
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+
+async function createPost(post) {
+  const newPost = await prisma.post.create({
+    data: {
+      title: post.title,
+      content: post.content,
+      isPublished: post.isPublished,
+      authorId: post.authorId,
+    },
+  });
+
+  return newPost;
+}
+
+async function getAllPosts() {
+  const posts = await prisma.post.findMany();
+
+  return posts;
+}
+
+async function getPostById(id) {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  return post;
+}
+
+async function getPostsByAuthorId(id) {
+  const posts = await prisma.post.findMany({
+    where: {
+      authorId: id,
+    },
+  });
+
+  return posts;
+}
+
+async function updatePost(post) {
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: post.id,
+    },
+    data: {
+      title: post.title,
+      content: post.content,
+      isPublished: post.isPublished,
+    },
+  });
+
+  return updatedPost;
+}
+
+async function deletePostById(id) {
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  return deletedPost;
+}
+
+export {
+  createPost,
+  getAllPosts,
+  getPostById,
+  getPostsByAuthorId,
+  updatePost,
+  deletePostById,
+};
